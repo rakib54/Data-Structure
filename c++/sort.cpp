ee7d39ed3fee5f1aa250a4e4277e356a20766e32
rakib54 <rakibur5454@gmail.com>
2022-07-14 00:30:26 +0600

    added sorted problem

--- c++/sort.cpp
+++ c++/sort.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  int arr[5] = {5, 3, 4, 2, 1};
+  for (int i = 0; i < 5 - 1; i++)
+  {
+    for (int j = i + 1; j < 5; j++)
+    {
+      if (arr[i] > arr[j])
+      {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  for (int i = 0; i < 5; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
